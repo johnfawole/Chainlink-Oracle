2f3cfae2b5dd754bd0311e0d64283317e356f5b6
blessingbblockchain <adeolasola01@gmail.coml>
2023-05-25 16:18:39 +0100

    Refactor tapping link contract

--- price feed.sol
+++ price feed.sol
@@ -4,10 +4,10 @@
    
    import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-   contract tappingChainlink{
+   contract TappingChainlink {
    AggregatorV3Interface internal priceFeed;
 
-   constructor()  {
+   constructor(address _priceFeedAddress)  {
     priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419);
 }
 
@@ -21,5 +21,5 @@
      ) = priceFeed.latestRoundData();
      return price;
     }
-    }
+   }
 
